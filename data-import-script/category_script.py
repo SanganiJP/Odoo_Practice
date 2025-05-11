7e3bce33b5016beeff1f5ceea33ccd3daa1c3dfe
sanganiJP <sanganijp212@gmail.com>
2025-05-11 17:03:30 +0530

    python script for data import in odoo database

---
+++ data-import-script/category_script.py
@@ -0,0 +1,94 @@
+# from openpyxl import load_workbook
+# import odoorpc
+#
+# # Prepare the connection to the server
+# odoo = odoorpc.ODOO('localhost', port=8069)
+#
+# # Login
+# odoo.login('HMS', 'admin', 'admin')
+#
+#
+# def import_product_categories():
+#     wb = load_workbook(filename='/home/jayesh/Downloads/Product Categories.xlsx')
+#     sheet = wb.active
+#     for record in sheet.iter_rows(min_row=2, max_row=None, min_col=None, max_col=None, values_only=True):
+#         parent_lst = []
+#         if record[1]:
+#             parent_cat = record[1].split(' / ')
+#             if len(parent_cat) == 1:
+#                 parent_category = odoo.env['product.category'].search([('name', '=', record[1])])
+#                 if parent_category:
+#                     parent_lst.append(parent_category[0])
+#                 else:
+#                     id = odoo.env['product.category'].create({
+#                         'name': record[0],
+#                     })
+#                     parent_lst.append(id)
+#
+#             if len(parent_cat) > 1:
+#                 parent_category = odoo.env['product.category'].search([('name', '=', parent_cat[len(parent_cat) - 1])])
+#                 sub_parent_id = odoo.env['product.category'].search([('name', '=', parent_cat[len(parent_cat) - 2])])
+#                 if parent_category:
+#                     parent_lst.append(parent_category[0])
+#                 else:
+#                     id = odoo.env['product.category'].create({
+#                         'name': parent_cat[len(parent_cat) - 1],
+#                         'parent_id': sub_parent_id[0]
+#                     })
+#                     parent_lst.append(id)
+#
+#         if record[0]:
+#             cat_list = record[0].split(' / ')
+#             cat_lst_length = len(cat_list)
+#             if cat_lst_length == 1:
+#                 category = odoo.env['product.category'].search([('name', '=', record[0])])
+#                 if category:
+#                     parent_lst.append(category[0])
+#                 else:
+#                     id = odoo.env['product.category'].create({
+#                         'name': record[0],
+#                     })
+#                     parent_lst.append(id[0])
+#
+#             if cat_lst_length > 1:
+#                 category = odoo.env['product.category'].search([('name', '=', cat_list[cat_lst_length - 1])])
+#                 if not category:
+#                     odoo.env['product.category'].create({
+#                         'name': cat_list[cat_lst_length - 1],
+#                         'parent_id': parent_lst[0]
+#                     })
+#
+#
+# import_product_categories()
+
+from openpyxl import load_workbook
+import odoorpc
+
+# Prepare the connection to the server
+odoo = odoorpc.ODOO('localhost', port=8069)
+odoo.login('T-snitch', 'admin', '123')
+
+
+def import_product_categories():
+    wb = load_workbook(filename='/home/jayesh/Downloads/Product Categories.xlsx')
+    sheet = wb.active
+
+    for record in sheet.iter_rows(min_row=2, values_only=True):
+        category_path = record[0]
+        if not category_path:
+            continue
+
+        parent_id = False
+        for name in category_path.split(' / '):
+            existing = odoo.env['product.category'].search([('name', '=', name), ('parent_id', '=', parent_id)], limit=1)
+            if existing:
+                parent_id = existing[0]
+            else:
+                parent_id = odoo.env['product.category'].create({
+                    'name': name,
+                    'parent_id': parent_id
+                })
+
+
+import_product_categories()
+
---
+++ data-import-script/script.py
@@ -0,0 +1,81 @@
+from openpyxl import load_workbook
+import odoorpc
+
+# Prepare the connection to the server
+odoo = odoorpc.ODOO('localhost', port=8069)
+
+# Login
+odoo.login('T-snitch', 'admin', '123')
+
+
+def import_work_center():
+    wb = load_workbook(filename='/home/jayesh/Downloads/Work Centers.xlsx')
+    sheet = wb.active
+    for record in sheet.iter_rows(min_row=2, max_row=None, min_col=None, max_col=None, values_only=True):
+        tags = record[1].split(',')
+        tag_list = []
+        for tag in tags:
+            tag_id = odoo.env['mrp.workcenter.tag'].search([('name', '=', tag)])
+            if tag_id:
+                tag_list.append(tag_id[0])
+            else:
+                tag_id = odoo.env['mrp.workcenter.tag'].create({
+                    'name': tag
+                })
+                tag_list.append(tag_id[0])
+
+        # alr_workcenter_list = []
+        # if record[3]:
+        #     alr_workcenters = record[3].split(',')
+        #     # print(alr_workcenters)
+        #     for center in alr_workcenters:
+        #         center_id = odoo.env['mrp.workcenter'].search([('name', '=', center)])
+        #         alr_workcenter_list.append(center_id[0])
+        #         if not search:
+        #             center_id = odoo.env['mrp.workcenter'].create({
+        #                     'name': center
+        #                 })
+        #             alr_workcenter_list.append(center_id[0])
+        #     print(alr_workcenter_list)
+
+        search = odoo.env['mrp.workcenter'].search([('name', '=', record[0])])
+        if not search:
+            odoo.env['mrp.workcenter'].create({
+                'name': record[0],
+                'tag_ids': [(6, 0, tag_list)],
+                'code': record[2],
+                # 'alternative_workcenter_ids': [(6,0,alr_workcenter_list)],
+            })
+
+    for record in sheet.iter_rows(min_row=2, max_row=None, min_col=None, max_col=None, values_only=True):
+        alr_workcenter_list = []
+        if record[3]:
+            alr_workcenters = record[3].split(',')
+            for center in alr_workcenters:
+                center_id = odoo.env['mrp.workcenter'].search([('name', '=', center)])
+                alr_workcenter_list.append(center_id[0])
+                print(alr_workcenter_list)
+
+        if len(alr_workcenter_list) > 0:
+            wc = odoo.env['mrp.workcenter'].search([('name', '=', record[0])])
+            center = odoo.env['mrp.workcenter'].browse(wc)
+            center.write({
+                'alternative_workcenter_ids': [(6, 0, alr_workcenter_list)],
+            })
+
+
+import_work_center()
+
+# alr_workcenter_list = []
+# if record[3]:
+#     alr_workcenters = record[3].split(',')
+#     # print(alr_workcenters)
+#     for center in alr_workcenters:
+#         search = odoo.env['mrp.workcenter'].search([('name', '=', center)])
+#         if not search:
+#             odoo.env['mrp.workcenter'].create({
+#                 'name': center,
+#             })
+#         center_id = odoo.env['mrp.workcenter'].search([('name', '=', center)])
+#         alr_workcenter_list.append(center_id[0])
+#     print(alr_workcenter_list)
